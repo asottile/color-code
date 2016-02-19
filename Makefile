e54545766e5e2ff50f19172af092bd6793354167
Anthony Sottile <asottile@yelp.com>
2016-02-19 10:44:16 -0800

    Add --widths option

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 .PHONY: all
-all: color_code.py
+all: ./color_code.py
 
 venv:
 	virtualenv venv -ppython2.7 && . venv/bin/activate && pip install yelp-cheetah
 
-color_code.py: venv color_code.tmpl
-	. venv/bin/activate && cheetah-compile color_code.tmpl
+./%.py: %.tmpl venv
+	. venv/bin/activate && cheetah-compile $^
 
 .PHONY: clean
 clean:
--- color-code.py
+++ color-code.py
@@ -9,12 +9,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
     parser.add_argument('output')
+    parser.add_argument('--widths', action='store_true')
     args = parser.parse_args()
 
     with io.open(args.output, 'wb') as output:
         subprocess.check_call(
             (sys.executable, '-m', 'color_code'),
-            env={'FILENAME': args.filename},
+            env={'FILENAME': args.filename, 'WIDTHS': str(args.widths)},
             stdout=output,
         )
 
