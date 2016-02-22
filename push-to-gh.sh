9931b5cb8d4e154f238660bbbf5745a87ec244a9
Anthony Sottile <asottile@umich.edu>
2016-02-21 21:59:58 -0800

    Add auto push-to-gh script

---
+++ push-to-gh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+[ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ] || exit
+
+git clone "https://${GH_TOKEN}@{GH_REF}" demo >& /dev/null
+
+pushd demo
+git checkout gh-pages
+git config user.name "Travis-CI"
+git config user.email "asottile@umich.edu"
+git rm *
+popd
+
+./venv/bin/python demo.py demo
+cp .travis.yml demo/
+
+cd demo
+git add .
+git commit -m "Deployed ${TRAVIS_BUILD_NUMBER} to Github Pages"
+git push -q origin HEAD >& /dev/null
